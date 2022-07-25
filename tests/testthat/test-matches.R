bbac7850ce43c78284fcd4f8f5af702a6d850779
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2022-07-25 15:42:19 -0400

    Create unit tests

---
+++ tests/testthat/test-matches.R
@@ -0,0 +1,12 @@
+test_that("uss_make_matches works", {
+  italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+  expect_true(tibble::is_tibble(italy))
+  expect_named(italy,
+               c("country", "tier", "season", "date",
+                  "home", "visitor", "goals_home",
+                  "goals_visitor"))
+  expect_identical(unique(italy$country), "Italy")
+  # if you find a bug, write a test
+  expect_s3_class(italy$tier, "factor")
+})
+
